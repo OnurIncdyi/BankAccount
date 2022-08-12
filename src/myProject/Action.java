071cd1d31da974264b7772f6a764f9ba8812080a
OnurIncdyi <108151328+OnurIncdyi@users.noreply.github.com>
2022-08-12 10:43:50 +0300

    Initial commit

---
+++ src/myProject/Action.java
@@ -0,0 +1,21 @@
+package myProject;
+
+public class Action {
+
+    private String actionName;
+    private int actionCode;
+
+    public Action(String actionName, int actionCode) {
+        this.actionName = actionName;
+        this.actionCode = actionCode;
+    }
+
+    public String getActionName() {
+        return actionName;
+    }
+
+    public int getActionCode() {
+        return actionCode;
+    }
+}
+
